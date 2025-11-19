8cab30a6e1d658977b26039e9c4b041633f3775b
Dawid Dykacz <69245647+dawiddykacz@users.noreply.github.com>
2025-11-19 14:23:01 +0100

    Update Range.py

--- objects/Range.py
+++ objects/Range.py
@@ -4,14 +4,14 @@ class Range:
         if min >= max:
             raise ValueError("Min must be less than or equal to max")
 
-        self.min = min
-        self.max = max
+        self.__min = min
+        self.__max = max
 
     def get_min(self):
-        return self.min
+        return self.__min
 
     def get_max(self):
-        return self.max
+        return self.__max
 
     def __str__(self):
-        return f'<{self.min};{self.max}>'
+        return f'<{self.__min};{self.__min}>'
