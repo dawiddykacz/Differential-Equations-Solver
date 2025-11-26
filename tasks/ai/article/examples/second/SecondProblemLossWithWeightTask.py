01920a5c06af414b2bd4839b48b5c259a7dfba23
Dawid Dykacz <69245647+dawiddykacz@users.noreply.github.com>
2025-11-26 17:27:13 +0100

    Update SecondProblemLossWithWeightTask.py

--- tasks/ai/article/examples/second/SecondProblemLossWithWeightTask.py
+++ tasks/ai/article/examples/second/SecondProblemLossWithWeightTask.py
@@ -4,7 +4,7 @@ from equations.ai.article.examples.second.SecondProblemLossWithWeight import Sec
 
 class SecondProblemLossWithWeightTask(TaskData):
     def __init__(self):
-        super().__init__(SpaceRanges(10, Range(0, 1)),"2 problem loss (weight)")
+        super().__init__(SpaceRanges(10, Range(0, 2)),"2 problem loss (weight)")
 
     def get_equation(self):
         return SecondProblemLossWithWeight(self.get_space_range().split())
