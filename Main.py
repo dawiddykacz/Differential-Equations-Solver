4815ab92222bb1555edba576cfee1fd5acff5f4a
ddykacz <dawiddykaczd@wp.pl>
2026-07-07 20:34:56 +0200

    added model with optimization

--- Main.py
+++ Main.py
@@ -16,14 +16,14 @@ def run_all(learning_rate: float):
     task_service = TaskService(task_repository)
     weight_plot_service = WeightPlotService(task_service.get_ms())
 
-    task_repository.add_task(FirstProblemLossTask())
+    #task_repository.add_task(FirstProblemLossTask())
     task_repository.add_task(FirstProblemSimpleTask())
-    task_repository.add_task(FifthProblemSimpleTask())
-    task_repository.add_task(FifthProblemLossTask())
+    #task_repository.add_task(FifthProblemSimpleTask())
+    #task_repository.add_task(FifthProblemLossTask())
     #task_repository.add_task(Second2ProblemLossTask())
     #task_repository.add_task(Second2ProblemLossWithNoiseTask())
     #task_repository.add_task(Fourth2EquationLossTask())
-    task_service.solve(5000)
+    task_service.solve(50000)
     weight_plot_service.plots(task_service.get_task_dict(), task_service.get_epochs())
 
     error_messages = task_service.get_error_messages()
--- solvers/models/ModelWithOptimization.py
+++ solvers/models/ModelWithOptimization.py
@@ -1,21 +1,53 @@
 import tensorflow
 import keras_tuner
-
 from solvers.models.AbstractModel import AbstractModel
 from objects.TrainableVariables import TrainableVariables
 
+import keras_tuner
+
+
+class MyRandomSearch(keras_tuner.RandomSearch):
+
+    def __init__(self, hypermodel, **kwargs):
+        super().__init__(hypermodel, **kwargs)
+
+        self._first_trial = True
+
+    def search(self, *args, **kwargs):
+        if self._first_trial:
+            self._first_trial = False
+
+            hp = keras_tuner.HyperParameters()
+
+            hp.values = {
+                "num_layers": 1,
+                "units_0": 10,
+                "activations_0": "sigmoid",
+                "learning_rate": 0.1,
+            }
+
+            trial = keras_tuner.engine.trial.Trial(
+                hyperparameters=hp,
+                trial_id="manual_default"
+            )
+
+            self.oracle.trials[trial.trial_id] = trial
+
+            self.run_trial(trial, *args, **kwargs)
+
+        return super().search(*args, **kwargs)
 
 class ModelWithOptimization(AbstractModel):
-    def __init__(self,wrapper, loss, trainable_variables: TrainableVariables, hp):
+    def __init__(self, wrapper, loss, trainable_variables: TrainableVariables, hp):
         self.__basic_loss = loss
         self.__wrapper = wrapper
 
-        num_layers = hp.Int('num_layers', min_value=1, max_value=5)
+        num_layers = hp.Int('num_layers', min_value=1, max_value=5, default=1)
         dense_layers = []
 
         for i in range(num_layers):
-            units = hp.Int('units_{}'.format(i), min_value=1, max_value=100, step=10)
-            activation = hp.Choice('activations_{}'.format(i), ['relu', 'tanh', 'sigmoid', 'swish'])
+            units = hp.Int('units_{}'.format(i), min_value=1, max_value=100, step=10, default=10)
+            activation = hp.Choice('activations_{}'.format(i), ['relu', 'tanh', 'sigmoid', 'swish'], default='sigmoid')
             dense_layers.append(tensorflow.keras.layers.Dense(units=units, activation=activation, dtype='float64'))
 
         super().__init__(
@@ -29,23 +61,22 @@ class ModelWithOptimization(AbstractModel):
         return self.__basic_loss()
 
 
-
 class ModelWithOptimizationWrapper:
     def __init__(self, loss, trainable_variables: TrainableVariables):
         self.__loss = loss
         self.__trainable_variables = trainable_variables
         self.__model = None
 
-        self.__tuner = keras_tuner.RandomSearch(
+        self.__tuner = MyRandomSearch(
             self.build_model,
             objective='loss',
-            max_trials=30,
+            max_trials=40,
             directory='pinn_tuning',
             project_name='eq',
             overwrite=True
         )
 
-    def init(self, inputs, epochs: int = 5000):
+    def init(self, inputs, epochs: int = 1000):
         self.__tuner.search_space_summary()
         self.__tuner.search(
             x=inputs,
@@ -67,7 +98,6 @@ class ModelWithOptimizationWrapper:
 
         self.__model(inputs)
 
-
     def set_current_model(self, model):
         self.__model = model
 
@@ -85,6 +115,6 @@ class ModelWithOptimizationWrapper:
             hp=hp
         )
 
-        lr = hp.Float('learning_rate', min_value=1e-4, max_value=1e-1, sampling='log')
+        lr = hp.Float('learning_rate', min_value=5e-3, max_value=1, sampling='log', default=0.1)
         model.compile(optimizer=tensorflow.keras.optimizers.Adam(learning_rate=lr))
         return model
