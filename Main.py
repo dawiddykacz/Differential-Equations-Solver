7860b93b2a9fdb4954bbcf971c6334990256465c
ddykacz <dawiddykaczd@wp.pl>
2026-08-18 22:23:44 +0200

    wang true true alpha 0.9 alpha lower 0.8

--- Main.py
+++ Main.py
@@ -43,15 +43,9 @@ def run_all(learning_rate: float):
     task_service = TaskService(task_repository)
     weight_plot_service = WeightPlotService(task_service.get_ms())
 
-    for with_noise in [True, False]:
-        for alpha in [1]:
-            for alpha_lower in [1, 0.95, 0.9, 0.85, 0.8]:
-                task_repository.add_task(
-                    ExampleFirst2ProblemLossWithWeightTask(alpha=alpha, alpha_lower=alpha_lower,
-                                                           with_noise=with_noise))
-                task_repository.add_task(
-                    ExampleSecond2ProblemLossTaskWithWeight(alpha=alpha, alpha_lower=alpha_lower,
-                                                            with_noise=with_noise))
+    task_repository.add_task(
+        ExampleSecond2ProblemLossTaskWithWeight(alpha=0.9, alpha_lower=0.8,
+                                                with_noise=True))
 
     task_service.solve(5000)
     weight_plot_service.plots(task_service.get_task_dict(), task_service.get_epochs())
