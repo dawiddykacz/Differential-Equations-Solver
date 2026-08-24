0da822bb8db1c2926c9336e914c109b4d88ec4b7
BuildTools <unconfigured@null.spigotmc.org>
2026-08-24 14:34:01 +0200

    2 test basic weights

--- Main.py
+++ Main.py
@@ -17,16 +17,21 @@ def configure_solver():
     model_configuration = ModelConfiguration()
     wang_params = WangParams()
     model_configuration.configure(model_with_optimization=None,
-                                  wang_configuration=wang_params)
+                                  wang_configuration=None,
+                                  dense_list=[
+                                      tensorflow.keras.layers.Dense(64, activation='tanh', dtype='float64'),
+                                      tensorflow.keras.layers.Dense(64, activation='tanh', dtype='float64'),
+                                      tensorflow.keras.layers.Dense(64, activation='tanh', dtype='float64')
+                                  ])
 
 
 def basic_rep(task_repository):
     for with_noise in [True, False]:
-        for weight in [1, 3, 5, 7, 10, 12, 15, 17, 20]:
+        for weight in [1, 5, 20]:
             task_repository.add_task(ExampleFirst2ProblemLossTask(weight=weight, with_noise=with_noise))
             task_repository.add_task(ExampleSecond2ProblemLossTask(weight=weight, with_noise=with_noise))
-        for alpha in [0.05, 0.1, 0.15, 0.2, 0.8, 0.9, 1]:
-            for alpha_lower in [1, 0.95, 0.9, 0.85, 0.8]:
+        for alpha in [0.1, 0.9, 1]:
+            for alpha_lower in [1, 0.8]:
                 task_repository.add_task(
                     ExampleFirst2ProblemLossWithWeightTask(alpha=alpha, alpha_lower=alpha_lower,
                                                            with_noise=with_noise))
@@ -43,15 +48,10 @@ def run_all(learning_rate: float):
     task_service = TaskService(task_repository)
     weight_plot_service = WeightPlotService(task_service.get_ms())
 
-    for with_noise in [False]:
-        for alpha in [0.05, 0.1]:
-            for alpha_lower in [1, 0.95, 0.9, 0.85, 0.8]:
-                task_repository.add_task(
-                    ExampleFirst2ProblemLossWithWeightTask(alpha=alpha, alpha_lower=alpha_lower,
-                                                           with_noise=with_noise))
-                task_repository.add_task(
-                    ExampleSecond2ProblemLossTaskWithWeight(alpha=alpha, alpha_lower=alpha_lower,
-                                                            with_noise=with_noise))
+    for with_noise in [True, False]:
+        for weight in [1, 5, 20]:
+            task_repository.add_task(ExampleFirst2ProblemLossTask(weight=weight, with_noise=with_noise))
+            task_repository.add_task(ExampleSecond2ProblemLossTask(weight=weight, with_noise=with_noise))
 
     task_service.solve(5000)
     weight_plot_service.plots(task_service.get_task_dict(), task_service.get_epochs())
@@ -64,4 +64,4 @@ def run_all(learning_rate: float):
 
 if __name__ == '__main__':
     configure_solver()
-    run_all(0.1)
+    run_all(0.001)
