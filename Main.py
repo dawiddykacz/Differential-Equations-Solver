0f3c65e93cd6c2affdd3eedc4d29bf3f9d3829ed
ddykacz <dawiddykaczd@wp.pl>
2026-08-29 10:32:13 +0200

    3 test wang true alpha 0.1, 0.9

--- Main.py
+++ Main.py
@@ -48,8 +48,8 @@ def run_all(learning_rate: float):
     task_service = TaskService(task_repository)
     weight_plot_service = WeightPlotService(task_service.get_ms())
 
-    for with_noise in [True, False]:
-        for alpha in [1]:
+    for with_noise in [True]:
+        for alpha in [0.1, 0.9]:
             for alpha_lower in [1, 0.95, 0.8]:
                 task_repository.add_task(
                     ExampleFirst2ProblemLossWithWeightTask(alpha=alpha, alpha_lower=alpha_lower,
