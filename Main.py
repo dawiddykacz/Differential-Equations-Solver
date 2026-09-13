a36ddc9180dab22ac3b0d45629da4ca4b14f9983
ddykacz <dawiddykaczd@wp.pl>
2026-09-13 15:46:26 +0200

    1 test basic alpha 0.1, 0.8

--- Main.py
+++ Main.py
@@ -43,8 +43,8 @@ def run_all(learning_rate: float):
     task_service = TaskService(task_repository)
     weight_plot_service = WeightPlotService(task_service.get_ms())
 
-    for with_noise in [False]:
-        for alpha in [0.85, 0.9]: #0.1, 0.8, 0.85, 0.9, 0.95, 1
+    for with_noise in [False, True]:
+        for alpha in [0.1, 0.8]: #0.1, 0.8, 0.85, 0.9, 0.95, 1
             for alpha_lower in [1, 0.95, 0.9, 0.8, 0.2, 0.15, 0.1, 0.05]:
                 task_repository.add_task(
                     ExampleFirst2ProblemLossWithWeightTask(alpha=alpha, alpha_lower=alpha_lower,
