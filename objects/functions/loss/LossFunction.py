7c635b49a1e83116f9eafd2ab0f18d82d1214807
BuildTools <unconfigured@null.spigotmc.org>
2026-09-18 11:59:56 +0200

    fix max_abs_grads

--- objects/functions/loss/LossFunction.py
+++ objects/functions/loss/LossFunction.py
@@ -58,7 +58,6 @@ class LossFunction(Function):
         return
 
     @staticmethod
-    @tensorflow.function
     def max_abs_grads(grad):
         valid_grads = [g for g in grad if g is not None]
 
@@ -69,7 +68,6 @@ class LossFunction(Function):
         return tensorflow.reduce_max(tensorflow.stack(max_values))
 
     @staticmethod
-    @tensorflow.function
     def mean_abs_grads(grad):
         valid_grads = [g for g in grad if g is not None]
 
